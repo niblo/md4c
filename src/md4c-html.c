9f6634d7039e5b2c007322c4c0d4d6c8159d7511
Martin Mitas <mity@morous.org>
2019-08-09 10:29:55 +0200

    Rename HTML renderer public identifier names.
    This is to reflect we make it a public API.

--- src/md4c-html.c
+++ src/md4c-html.c
@@ -44,8 +44,8 @@
 
 
 
-typedef struct MD_RENDER_HTML_tag MD_RENDER_HTML;
-struct MD_RENDER_HTML_tag {
+typedef struct MD_HTML_tag MD_HTML;
+struct MD_HTML_tag {
     void (*process_output)(const MD_CHAR*, MD_SIZE, void*);
     void* userdata;
     unsigned flags;
@@ -65,7 +65,7 @@ struct MD_RENDER_HTML_tag {
 
 
 static inline void
-render_text(MD_RENDER_HTML* r, const MD_CHAR* text, MD_SIZE size)
+render_text(MD_HTML* r, const MD_CHAR* text, MD_SIZE size)
 {
     r->process_output(text, size, r->userdata);
 }
@@ -74,7 +74,7 @@ render_text(MD_RENDER_HTML* r, const MD_CHAR* text, MD_SIZE size)
 
 
 static void
-render_html_escaped(MD_RENDER_HTML* r, const MD_CHAR* data, MD_SIZE size)
+render_html_escaped(MD_HTML* r, const MD_CHAR* data, MD_SIZE size)
 {
     MD_OFFSET beg = 0;
     MD_OFFSET off = 0;
@@ -109,7 +109,7 @@ render_html_escaped(MD_RENDER_HTML* r, const MD_CHAR* data, MD_SIZE size)
 }
 
 static void
-render_url_escaped(MD_RENDER_HTML* r, const MD_CHAR* data, MD_SIZE size)
+render_url_escaped(MD_HTML* r, const MD_CHAR* data, MD_SIZE size)
 {
     static const MD_CHAR hex_chars[] = "0123456789ABCDEF";
     MD_OFFSET beg = 0;
@@ -158,8 +158,8 @@ hex_val(char ch)
 }
 
 static void
-render_utf8_codepoint(MD_RENDER_HTML* r, unsigned codepoint,
-                      void (*fn_append)(MD_RENDER_HTML*, const MD_CHAR*, MD_SIZE))
+render_utf8_codepoint(MD_HTML* r, unsigned codepoint,
+                      void (*fn_append)(MD_HTML*, const MD_CHAR*, MD_SIZE))
 {
     static const MD_CHAR utf8_replacement_char[] = { 0xef, 0xbf, 0xbd };
 
@@ -195,10 +195,10 @@ render_utf8_codepoint(MD_RENDER_HTML* r, unsigned codepoint,
 /* Translate entity to its UTF-8 equivalent, or output the verbatim one
  * if such entity is unknown (or if the translation is disabled). */
 static void
-render_entity(MD_RENDER_HTML* r, const MD_CHAR* text, MD_SIZE size,
-              void (*fn_append)(MD_RENDER_HTML*, const MD_CHAR*, MD_SIZE))
+render_entity(MD_HTML* r, const MD_CHAR* text, MD_SIZE size,
+              void (*fn_append)(MD_HTML*, const MD_CHAR*, MD_SIZE))
 {
-    if(r->flags & MD_RENDER_FLAG_VERBATIM_ENTITIES) {
+    if(r->flags & MD_HTML_FLAG_VERBATIM_ENTITIES) {
         fn_append(r, text, size);
         return;
     }
@@ -238,8 +238,8 @@ render_entity(MD_RENDER_HTML* r, const MD_CHAR* text, MD_SIZE size,
 }
 
 static void
-render_attribute(MD_RENDER_HTML* r, const MD_ATTRIBUTE* attr,
-                 void (*fn_append)(MD_RENDER_HTML*, const MD_CHAR*, MD_SIZE))
+render_attribute(MD_HTML* r, const MD_ATTRIBUTE* attr,
+                 void (*fn_append)(MD_HTML*, const MD_CHAR*, MD_SIZE))
 {
     int i;
 
@@ -259,7 +259,7 @@ render_attribute(MD_RENDER_HTML* r, const MD_ATTRIBUTE* attr,
 
 
 static void
-render_open_ol_block(MD_RENDER_HTML* r, const MD_BLOCK_OL_DETAIL* det)
+render_open_ol_block(MD_HTML* r, const MD_BLOCK_OL_DETAIL* det)
 {
     char buf[64];
 
@@ -273,7 +273,7 @@ render_open_ol_block(MD_RENDER_HTML* r, const MD_BLOCK_OL_DETAIL* det)
 }
 
 static void
-render_open_li_block(MD_RENDER_HTML* r, const MD_BLOCK_LI_DETAIL* det)
+render_open_li_block(MD_HTML* r, const MD_BLOCK_LI_DETAIL* det)
 {
     if(det->is_task) {
         RENDER_LITERAL(r, "<li class=\"task-list-item\">"
@@ -287,7 +287,7 @@ render_open_li_block(MD_RENDER_HTML* r, const MD_BLOCK_LI_DETAIL* det)
 }
 
 static void
-render_open_code_block(MD_RENDER_HTML* r, const MD_BLOCK_CODE_DETAIL* det)
+render_open_code_block(MD_HTML* r, const MD_BLOCK_CODE_DETAIL* det)
 {
     RENDER_LITERAL(r, "<pre><code");
 
@@ -302,7 +302,7 @@ render_open_code_block(MD_RENDER_HTML* r, const MD_BLOCK_CODE_DETAIL* det)
 }
 
 static void
-render_open_td_block(MD_RENDER_HTML* r, const MD_CHAR* cell_type, const MD_BLOCK_TD_DETAIL* det)
+render_open_td_block(MD_HTML* r, const MD_CHAR* cell_type, const MD_BLOCK_TD_DETAIL* det)
 {
     RENDER_LITERAL(r, "<");
     RENDER_LITERAL(r, cell_type);
@@ -316,7 +316,7 @@ render_open_td_block(MD_RENDER_HTML* r, const MD_CHAR* cell_type, const MD_BLOCK
 }
 
 static void
-render_open_a_span(MD_RENDER_HTML* r, const MD_SPAN_A_DETAIL* det)
+render_open_a_span(MD_HTML* r, const MD_SPAN_A_DETAIL* det)
 {
     RENDER_LITERAL(r, "<a href=\"");
     render_attribute(r, &det->href, render_url_escaped);
@@ -330,7 +330,7 @@ render_open_a_span(MD_RENDER_HTML* r, const MD_SPAN_A_DETAIL* det)
 }
 
 static void
-render_open_img_span(MD_RENDER_HTML* r, const MD_SPAN_IMG_DETAIL* det)
+render_open_img_span(MD_HTML* r, const MD_SPAN_IMG_DETAIL* det)
 {
     RENDER_LITERAL(r, "<img src=\"");
     render_attribute(r, &det->src, render_url_escaped);
@@ -341,7 +341,7 @@ render_open_img_span(MD_RENDER_HTML* r, const MD_SPAN_IMG_DETAIL* det)
 }
 
 static void
-render_close_img_span(MD_RENDER_HTML* r, const MD_SPAN_IMG_DETAIL* det)
+render_close_img_span(MD_HTML* r, const MD_SPAN_IMG_DETAIL* det)
 {
     if(det->title.text != NULL) {
         RENDER_LITERAL(r, "\" title=\"");
@@ -362,7 +362,7 @@ static int
 enter_block_callback(MD_BLOCKTYPE type, void* detail, void* userdata)
 {
     static const MD_CHAR* head[6] = { "<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>" };
-    MD_RENDER_HTML* r = (MD_RENDER_HTML*) userdata;
+    MD_HTML* r = (MD_HTML*) userdata;
 
     switch(type) {
         case MD_BLOCK_DOC:      /* noop */ break;
@@ -390,7 +390,7 @@ static int
 leave_block_callback(MD_BLOCKTYPE type, void* detail, void* userdata)
 {
     static const MD_CHAR* head[6] = { "</h1>\n", "</h2>\n", "</h3>\n", "</h4>\n", "</h5>\n", "</h6>\n" };
-    MD_RENDER_HTML* r = (MD_RENDER_HTML*) userdata;
+    MD_HTML* r = (MD_HTML*) userdata;
 
     switch(type) {
         case MD_BLOCK_DOC:      /*noop*/ break;
@@ -417,7 +417,7 @@ leave_block_callback(MD_BLOCKTYPE type, void* detail, void* userdata)
 static int
 enter_span_callback(MD_SPANTYPE type, void* detail, void* userdata)
 {
-    MD_RENDER_HTML* r = (MD_RENDER_HTML*) userdata;
+    MD_HTML* r = (MD_HTML*) userdata;
 
     if(r->image_nesting_level > 0) {
         /* We are inside an image, i.e. rendering the ALT attribute of
@@ -442,7 +442,7 @@ enter_span_callback(MD_SPANTYPE type, void* detail, void* userdata)
 static int
 leave_span_callback(MD_SPANTYPE type, void* detail, void* userdata)
 {
-    MD_RENDER_HTML* r = (MD_RENDER_HTML*) userdata;
+    MD_HTML* r = (MD_HTML*) userdata;
 
     if(r->image_nesting_level > 0) {
         /* We are inside an image, i.e. rendering the ALT attribute of
@@ -469,7 +469,7 @@ leave_span_callback(MD_SPANTYPE type, void* detail, void* userdata)
 static int
 text_callback(MD_TEXTTYPE type, const MD_CHAR* text, MD_SIZE size, void* userdata)
 {
-    MD_RENDER_HTML* r = (MD_RENDER_HTML*) userdata;
+    MD_HTML* r = (MD_HTML*) userdata;
 
     switch(type) {
         case MD_TEXT_NULLCHAR:  render_utf8_codepoint(r, 0x0000, render_text); break;
@@ -486,17 +486,17 @@ text_callback(MD_TEXTTYPE type, const MD_CHAR* text, MD_SIZE size, void* userdat
 static void
 debug_log_callback(const char* msg, void* userdata)
 {
-    MD_RENDER_HTML* r = (MD_RENDER_HTML*) userdata;
-    if(r->flags & MD_RENDER_FLAG_DEBUG)
+    MD_HTML* r = (MD_HTML*) userdata;
+    if(r->flags & MD_HTML_FLAG_DEBUG)
         fprintf(stderr, "MD4C: %s\n", msg);
 }
 
 int
-md_render_html(const MD_CHAR* input, MD_SIZE input_size,
-               void (*process_output)(const MD_CHAR*, MD_SIZE, void*),
-               void* userdata, unsigned parser_flags, unsigned renderer_flags)
+md_html(const MD_CHAR* input, MD_SIZE input_size,
+        void (*process_output)(const MD_CHAR*, MD_SIZE, void*),
+        void* userdata, unsigned parser_flags, unsigned renderer_flags)
 {
-    MD_RENDER_HTML render = { process_output, userdata, renderer_flags, 0, { 0 } };
+    MD_HTML render = { process_output, userdata, renderer_flags, 0, { 0 } };
 
     MD_PARSER parser = {
         0,
